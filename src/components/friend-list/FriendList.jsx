f1c2c19d44dfd33ebc8bbd5994e70f17114277d2
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2022-10-23 11:41:09 +0300

    Create FriendList.jsx

---
+++ src/components/friend-list/FriendList.jsx
@@ -0,0 +1,7 @@
+import { FriendListItem } from "./friends-list-item/FriendListItem"
+export const FriendsList = () => {
+  return <ul class="friend-list">
+    <FriendListItem />
+  {/* Довільна кіл-сть FriendListItem */}
+</ul>
+}
