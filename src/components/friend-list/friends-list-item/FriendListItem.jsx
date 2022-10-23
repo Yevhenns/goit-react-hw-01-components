6eb4a5a5e0542ce28a491ca077413f5f4be18734
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2022-10-23 17:44:11 +0300

    Update FriendListItem.jsx

--- src/components/friend-list/friends-list-item/FriendListItem.jsx
+++ src/components/friend-list/friends-list-item/FriendListItem.jsx
@@ -1,7 +1,7 @@
-export const FriendListItem = ({avatar, name, isOnline, key}) => {
-  return <li class="item" key={key}>
+export const FriendListItem = ({ avatar, name, isOnline }) => {
+  return <li class="item">
     <span class="status">{isOnline}</span>
   <img class="avatar" src={avatar} alt="User avatar" width="48" />
     <p class="name">{name}</p>
 </li>
-  }
+}
