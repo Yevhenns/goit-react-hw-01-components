dea4f8910b3feec3da2a108abb1e6964c4b3c4de
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2022-10-23 15:24:27 +0300

    Update Statistics.jsx

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -1,6 +1,8 @@
-export const Statistics = () => {
+// import PropTypes from 'prop-types'
+
+export const Statistics = ({title}) => {
   return <section class="statistics">
-  <h2 class="title">Upload stats</h2>
+    <h2 class="title">{title}</h2>
 
   <ul class="stat-list">
     <li class="item">
@@ -22,3 +24,15 @@ export const Statistics = () => {
   </ul>
 </section>
 }
+
+// Profile.propTypes = {
+//   username: PropTypes.string.isRequired,
+//   tag: PropTypes.string.isRequired,
+//   location: PropTypes.string.isRequired,
+//   avatar: PropTypes.string.isRequired, 
+//   stats: PropTypes.shape(
+//    { followers: PropTypes.number.isRequired,
+//     views: PropTypes.number.isRequired,
+//     likes: PropTypes.number.isRequired,
+//   }).isRequired,
+// }
