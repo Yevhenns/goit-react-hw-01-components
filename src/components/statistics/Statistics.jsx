fbee1a4798cb4a3fe1370610cf3e8821580ecc26
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2022-10-23 18:06:17 +0300

    Update Statistics.jsx

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -1,11 +1,11 @@
 export const Statistics = ({title, stats}) => {
-  return <section class="statistics">
-    <h2 class="title">{title}</h2>
-    <ul class="stat-list">
+  return <section className="statistics">
+    <h2 className="title">{title}</h2>
+    <ul className="stat-list">
       {stats.map(element => {
-        return <li class="item" key={element.id}>
-      <span class="label">{element.label}</span>
-      <span class="percentage">{element.percentage}%</span>
+        return <li className="item" key={element.id}>
+      <span className="label">{element.label}</span>
+      <span className="percentage">{element.percentage}%</span>
     </li>
       })}
   </ul>
