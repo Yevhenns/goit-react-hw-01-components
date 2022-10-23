e3d20ae61e1a8e26c3e4d71d268d4531d216a925
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2022-10-23 11:41:07 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import { Profile } from "./components/profile/Profile";
+import { Statistics } from "components/statistics/Statistics";
+import { FriendsList } from "components/friend-list/FriendList";
+import { TransactionHistory } from "components/transaction-history/TransactionHistory";
+
 export const App = () => {
   return (
     <div>
       <Profile />
+      <Statistics />
+      <FriendsList />
+      <TransactionHistory />
     </div>
   );
 };
