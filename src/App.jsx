1eb6c791796e0cb9ac9beefd52512df14c99c37b
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2022-10-23 15:24:21 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,22 @@ import { Statistics } from "components/statistics/Statistics";
 import { FriendsList } from "components/friend-list/FriendList";
 import { TransactionHistory } from "components/transaction-history/TransactionHistory";
 
+import user from './data/user.json';
+import data from './data/data.json'
+
 export const App = () => {
   return (
     <div>
-      <Profile />
-      <Statistics />
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}/>
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
       <FriendsList />
       <TransactionHistory />
     </div>
