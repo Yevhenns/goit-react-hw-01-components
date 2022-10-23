a305e5ce78fc729192c5118736e8c1abf46d8b3d
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2022-10-23 15:42:20 +0300

    Update Statistics.jsx

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -1,38 +1,14 @@
-// import PropTypes from 'prop-types'
-
-export const Statistics = ({title}) => {
+export const Statistics = ({title, stats}) => {
   return <section class="statistics">
     <h2 class="title">{title}</h2>
 
-  <ul class="stat-list">
-    <li class="item">
-      <span class="label">.docx</span>
-      <span class="percentage">4%</span>
+    <ul class="stat-list">
+      {stats.map((element, index) => {
+        return <li class="item">
+      <span class="label" key={element.id}>{element.label}</span>
+      <span class="percentage">{element.percentage}%</span>
     </li>
-    <li class="item">
-      <span class="label">.mp3</span>
-      <span class="percentage">14%</span>
-    </li>
-    <li class="item">
-      <span class="label">.pdf</span>
-      <span class="percentage">41%</span>
-    </li>
-    <li class="item">
-      <span class="label">.mp4</span>
-      <span class="percentage">12%</span>
-    </li>
+      })}
   </ul>
 </section>
 }
-
-// Profile.propTypes = {
-//   username: PropTypes.string.isRequired,
-//   tag: PropTypes.string.isRequired,
-//   location: PropTypes.string.isRequired,
-//   avatar: PropTypes.string.isRequired, 
-//   stats: PropTypes.shape(
-//    { followers: PropTypes.number.isRequired,
-//     views: PropTypes.number.isRequired,
-//     likes: PropTypes.number.isRequired,
-//   }).isRequired,
-// }
