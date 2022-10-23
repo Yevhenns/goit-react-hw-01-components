330662cc70e5ec588dae279940ffa090f8b06502
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2022-10-23 17:44:15 +0300

    Update Statistics.jsx

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -3,8 +3,8 @@ export const Statistics = ({title, stats}) => {
     <h2 class="title">{title}</h2>
     <ul class="stat-list">
       {stats.map(element => {
-        return <li class="item">
-      <span class="label" key={element.id}>{element.label}</span>
+        return <li class="item" key={element.id}>
+      <span class="label">{element.label}</span>
       <span class="percentage">{element.percentage}%</span>
     </li>
       })}
